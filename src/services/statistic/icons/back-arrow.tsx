26d71ec421b0b2b30b543578615001bee0606c5e
gnafhan <nafhanghifari@gmail.com>
2025-02-18 12:30:20 +0700

    feat: migrate statistic services

---
+++ src/services/statistic/icons/back-arrow.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+interface BackIconProps {
+  className?: string;
+}
+
+const BackIcon: React.FC<BackIconProps> = ({ className = '' }) => {
+  return (
+    <svg
+      width="42"
+      height="42"
+      viewBox="0 0 42 42"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      <g id="Arrow_left_long">
+        <path
+          id="Vector 9"
+          d="M7 12H21"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          id="Vector 10"
+          d="M2.28481 11.7966L7.73501 7.90356C8.2645 7.52535 9 7.90385 9 8.55455V15.4454C9 16.0961 8.2645 16.4746 7.73501 16.0964L2.28481 12.2034C2.14522 12.1037 2.14522 11.8963 2.28481 11.7966Z"
+          fill="currentColor"
+        />
+      </g>
+    </svg>
+  );
+};
+
+export default BackIcon;
---
+++ src/services/statistic/pages/statistic_page.tsx
@@ -0,0 +1,110 @@
+import React, { useState } from 'react';
+import AppMobileLayout from '@/layout/app_mobile_layout';
+import { Card } from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
+import Chart from '../../home/components/chart';
+import { dailyCalorieData, weeklyCalorieData, monthlyCalorieData } from '../../home/data/chart_data';
+import { dailyWeightData, weeklyWeightData, monthlyWeightData } from '../../home/data/chart_data';
+import BackIcon from '../icons/back-arrow';
+import Head from 'next/head';
+import Link from 'next/link';
+
+interface Props {
+  user?: {
+    name: string;
+    isPro: boolean;
+    points: number;
+  };
+  backUrl: string;
+}
+
+export default function Statistics({ user, backUrl= '/' }: Props) {
+  const [caloriePeriod, setCaloriePeriod] = useState<'daily' | 'weekly' | 'monthly'>('daily');
+  const [weightPeriod, setWeightPeriod] = useState<'daily' | 'weekly' | 'monthly'>('daily');
+
+  const getCalorieData = () => {
+    switch (caloriePeriod) {
+      case 'daily':
+        return dailyCalorieData;
+      case 'weekly':
+        return weeklyCalorieData;
+      case 'monthly':
+        return monthlyCalorieData;
+    }
+  };
+
+  const getWeightData = () => {
+    switch (weightPeriod) {
+      case 'daily':
+        return dailyWeightData;
+      case 'weekly':
+        return weeklyWeightData;
+      case 'monthly':
+        return monthlyWeightData;
+    }
+  };
+
+  const getCalorieTitle = () => {
+    switch (caloriePeriod) {
+      case 'daily':
+        return 'Harian';
+      case 'weekly':
+        return 'Mingguan';
+      case 'monthly':
+        return 'Bulanan';
+    }
+  };
+
+  const getWeightTitle = () => {
+    switch (weightPeriod) {
+      case 'daily':
+        return 'Harian';
+      case 'weekly':
+        return 'Mingguan';
+      case 'monthly':
+        return 'Bulanan';
+    }
+  };
+
+  return (
+    <AppMobileLayout>
+      <Head>
+        <title>Statistik | Nutribox</title>
+      </Head>
+      <div className="w-full relative flex flex-col outfit-font pb-20 bg-[#3E9295]">
+        <img src="/assets/img/home-pattern.png" className="w-full absolute -z-0 top-0 left-0 h-full object-cover opacity-10" alt="pattern" />
+        <Link href={backUrl} className='ms-8 cursor-pointer'>
+          <BackIcon className="w-10 h-10 mt-5 whi fill-white text-white cursor-pointer" />
+        </Link>
+        <div className="w-full p-4">
+          <div className="rounded-xl px-4 shadow-sm">
+            <Chart
+              title={`Kalori ${getCalorieTitle()}`}
+              data={getCalorieData()}
+              period={caloriePeriod}
+              onPeriodChange={setCaloriePeriod}
+              metricKey="calories"
+              metricLabel="Kalori"
+              metricColor="#53C2C6"
+              isDetail
+            />
+            
+          </div>
+
+          <div className="rounded-xl p-4 shadow-sm">
+            <Chart
+              title={`Berat ${getWeightTitle()}`}
+              data={getWeightData()}
+              period={weightPeriod}
+              onPeriodChange={setWeightPeriod}
+              metricKey="weight"
+              metricLabel="Berat"
+              metricColor="#53C2C6"
+              isDetail
+            />
+          </div>
+        </div>
+      </div>
+    </AppMobileLayout>
+  );
+}
